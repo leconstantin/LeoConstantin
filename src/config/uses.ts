e8d39b7f1c06f339c5a225ca551c528df88487fc
leconstantin <lcon69184@gmail.com>
2025-01-14 21:14:30 +0200

    updated uses

--- src/config/uses.ts
+++ src/config/uses.ts
@@ -5,14 +5,14 @@ export const uses: TUses[] = [
     category: "Dev",
     items: [
       {
-        title: "Sublime Text",
+        title: "VS Code",
         subtitle:
-          "I was using an Intel-based 16” MacBook Pro prior to this and the difference is night and day. I've never heard the fans turn on a single time, even under the incredibly heavy loads I put it through with our various launch simulations.",
+          "Visual Studio Code (VS Code) is a lightweight yet powerful code editor developed by Microsoft, known for its speed and versatility. It supports a wide range of programming languages and offers features like IntelliSense, debugging, and Git integration.",
       },
       {
-        title: "16 MacBook Pro, M1 Max, 64GB RAM (2021)",
+        title: "Node.js",
         subtitle:
-          "I was using an Intel-based 16” MacBook Pro prior to this and the difference is night and day. I've never heard the fans turn on a single time, even under the incredibly heavy loads I put it through with our various launch simulations.",
+          "Node.js is a runtime environment that allows developers to run JavaScript on the server side, built on Chrome's V8 engine. It is known for its non-blocking, event-driven architecture, making it ideal for building scalable, real-time applications.",
       },
     ],
   },
@@ -22,7 +22,12 @@ export const uses: TUses[] = [
       {
         title: "Figma",
         subtitle:
-          "I was using an Intel-based 16” MacBook Pro prior to this and the difference is night and day. I've never heard the fans turn on a single time, even under the incredibly heavy loads I put it through with our various launch simulations.",
+          "Figma is a super-friendly design tool that makes creating stunning user interfaces and prototypes a breeze. It's cloud-based, so teams can collaborate in real-time, whether you're tweaking layouts or brainstorming ideas.",
+      },
+      {
+        title: "Relume",
+        subtitle:
+          "Relume is a game-changer for web designers and developers, offering a library of pre-designed, Webflow-ready components to speed up your workflow. It’s perfect for building beautiful websites without starting from scratch, and it integrates seamlessly with tools like Figma. ",
       },
     ],
   },
@@ -32,22 +37,12 @@ export const uses: TUses[] = [
       {
         title: "Notion",
         subtitle:
-          "I was using an Intel-based 16” MacBook Pro prior to this and the difference is night and day. I've never heard the fans turn on a single time, even under the incredibly heavy loads I put it through with our various launch simulations.",
+          "Notion is your all-in-one workspace for organizing your life and work, blending notes, tasks, and projects into one seamless tool. It’s super flexible, letting you create anything from to-do lists to full-blown project dashboards. ",
       },
       {
-        title: "App Eraser",
+        title: "Eraser",
         subtitle:
-          "I was using an Intel-based 16” MacBook Pro prior to this and the difference is night and day. I've never heard the fans turn on a single time, even under the incredibly heavy loads I put it through with our various launch simulations.",
-      },
-      {
-        title: "SavvyCal",
-        subtitle:
-          "I was using an Intel-based 16” MacBook Pro prior to this and the difference is night and day. I've never heard the fans turn on a single time, even under the incredibly heavy loads I put it through with our various launch simulations.",
-      },
-      {
-        title: "Focus",
-        subtitle:
-          "I was using an Intel-based 16” MacBook Pro prior to this and the difference is night and day. I've never heard the fans turn on a single time, even under the incredibly heavy loads I put it through with our various launch simulations.",
+          "Eraser is a simple and privacy-first note-taking app designed to make collaboration effortless. It lets you jot down thoughts, brainstorm ideas, and share them instantly with others in real-time.",
       },
     ],
   },
